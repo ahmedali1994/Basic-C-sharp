000d037dd26f8dc637d7eb3c57da58dac3e5cfd4
ahmedali1994 <33011828+ahmedali1994@users.noreply.github.com>
2018-02-20 15:24:31 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter you name");
+            string Userinpt = Console.ReadLine();
+            Userinpt = Userinpt.ToUpper();
+            char[] inputtochar = Userinpt.ToCharArray();
+            Array.Reverse(inputtochar);
+            string Reverse = new string(inputtochar);
+            
+
+
+            foreach (char A in Reverse )
+            {
+                Console.Write("{0} ", A);
+
+
+                
+            }
+            Console.ReadKey();
+            
+
+
+
+
+
+
+
+
+        }
+        
+    }
+}
